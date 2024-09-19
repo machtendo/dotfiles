1ef66770e39cd8559e54dce65906dcc3e106a3fe
machtendo <93202234+machtendo@users.noreply.github.com>
2024-09-19 08:10:29 -0500

    disable mv commands for sourcing config from git

--- dotfiles-link.sh
+++ dotfiles-link.sh
@@ -7,17 +7,17 @@ mkdir -p $HOME/.dotfiles/nixos
 # Move Configuration Files to 'dotfiles'
 
     # NixOS Configuration Files
-    echo "Moving NixOS Configuration Files..."
+    # echo "Moving NixOS Configuration Files..."
 
-    sudo mv /etc/nixos/configuration.nix $HOME/dotfiles/nixos/
-    sudo mv /etc/nixos/hardware-configuration.nix $HOME/dotfiles/nixos/
+    # sudo mv /etc/nixos/configuration.nix $HOME/dotfiles/nixos/
+    # sudo mv /etc/nixos/hardware-configuration.nix $HOME/dotfiles/nixos/
 
     # NixOS Home Manager Configuration Files
-    echo "Moving Home Manager Configuration Files..."
+    # echo "Moving Home Manager Configuration Files..."
 
-    mv $HOME/.config/home-manager/home.nix
-    mv $HOME/.config/home-manager/flake.nix
-    mv $HOME/.config/home-manager/flake.lock
+    # mv $HOME/.config/home-manager/home.nix
+    # mv $HOME/.config/home-manager/flake.nix
+    # mv $HOME/.config/home-manager/flake.lock
 
 # Create Links
 
