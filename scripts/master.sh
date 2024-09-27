2da17da774c8e6005a76fc07afb679fcc794ad28
machtendo <93202234+machtendo@users.noreply.github.com>
2024-09-27 09:02:10 -0500

    Consolidate master script

--- scripts/master.sh
+++ scripts/master.sh
@@ -1,6 +1,5 @@
 #! /bin/bash
 
-
 # NixOS Unstable Channel
 
     echo "Switching to NixOS Unstable Channel..."
@@ -27,7 +26,40 @@
     # Install Home Manager
     nix-shell '<home-manager>' -A install
 
-# Download Dotfiles from Github
+# Install Plasma Manager - Needs Testing
+
+    #    echo "Installing Plasma Manager..."
+    #
+    #    # Add Plasma Manager Channel
+    #    sudo nix-channel --add https://github.com/nix-community/plasma-manager/archive/trunk.tar.gz plasma-manager
+    #
+    #    # Update Channels
+    #    sudo nix-channel --update plasma-manager
+    #
+    #    # Install Home Manager
+    #    nix-shell '<plasma-manager>' -A install
+
+# Initial Dotfiles Creation - Needs If/Then Check
+
+    # Create Dotfiles Folder in HOME
+
+    #    mkdir -p ~/.dotfiles
+
+    #    # NixOS Configuration Files
+    
+    #    echo "Moving NixOS Configuration Files..."
+
+    #    sudo mv /etc/nixos/configuration.nix ~/.dotfiles/nixos/
+    #    sudo mv /etc/nixos/hardware-configuration.nix ~/.dotfiles/nixos/
+
+    #    # NixOS Home Manager Configuration Files
+    #    echo "Moving Home Manager Configuration Files..."
+
+    #    mv ~/.config/home-manager/home.nix ~/.dotfiles/nixos/
+    #    mv ~/.config/home-manager/flake.nix ~/.dotfiles/nixos/
+    #    mv ~/.config/home-manager/flake.lock ~/.dotfiles/nixos/
+
+# Download Dotfiles from Github - Done Manually
     #    # Create Temporary Shell Using Git
     #    nix-shell -p git
     #    # Ensure Home Directory
@@ -44,14 +76,17 @@
 # Link Dotfiles
 
     # Create Links - NixOS Configuration Files
+    
     echo "Setting NixOS Soft Links..."
     sudo ln -sf ~/.dotfiles/nixos/configuration.nix /etc/nixos/configuration.nix
 
-    # Copy & Link hardware-configuration.nix
+    # Move & Link hardware-configuration.nix
+    
     sudo mv /etc/nixos/hardware-configuration.nix ~/.dotfiles/nixos/hardware-configuration.nix
     sudo ln -sf ~/.dotfiles/nixos/hardware-configuration.nix /etc/nixos/hardware-configuration.nix
 
     # NixOS Home Manager Configuration Files
+    
     echo "Setting Home Manager Soft Links..."
 
     ln -sf ~/.dotfiles/nixos/home.nix ~/.config/home-manager/home.nix
@@ -63,3 +98,7 @@
     echo "Final NixOS Rebuild..."
 
     sudo nixos-rebuild switch
+
+# End
+
+    echo "Your device is configured. Reboot Recommended."
