e500c251f8cb7c5759fba7f5dd68d036bd966d64
machtendo <93202234+machtendo@users.noreply.github.com>
2024-10-30 10:39:49 -0500

    nvidia configuration for lenovo legion

--- nixos/endpoints/Lenovo Legion 9i.nix
+++ nixos/endpoints/Lenovo Legion 9i.nix
@@ -11,8 +11,63 @@
     ];
 
 # Drivers
-# Lenovo Legion - Power Management, Fan Control, RGB, 
-linuxKernel.packages.linux_latest_libre.lenovo-legion-module
+
+# nVidia - https://nixos.wiki/wiki/Nvidia
+
+  # Enable OpenGL
+  hardware.opengl = {
+    enable = true;
+  };
+
+  # Load nvidia driver for Xorg and Wayland
+  services.xserver.videoDrivers = ["nvidia"];
+
+  hardware.nvidia = {
+
+    # Modesetting is required.
+    modesetting.enable = true;
+
+    # Nvidia power management. Experimental, and can cause sleep/suspend to fail.
+    # Enable this if you have graphical corruption issues or application crashes after waking
+    # up from sleep. This fixes it by saving the entire VRAM memory to /tmp/ instead 
+    # of just the bare essentials.
+    powerManagement.enable = false;
+
+    # Fine-grained power management. Turns off GPU when not in use.
+    # Experimental and only works on modern Nvidia GPUs (Turing or newer).
+    powerManagement.finegrained = false;
+
+    # Use the NVidia open source kernel module (not to be confused with the
+    # independent third-party "nouveau" open source driver).
+    # Support is limited to the Turing and later architectures. Full list of 
+    # supported GPUs is at: 
+    # https://github.com/NVIDIA/open-gpu-kernel-modules#compatible-gpus 
+    # Only available from driver 515.43.04+
+    # Currently alpha-quality/buggy, so false is currently the recommended setting.
+    open = false;
+
+    # Enable the Nvidia settings menu, accessible via `nvidia-settings`.
+    nvidiaSettings = true;
+
+    # Optionally, you may need to select the appropriate driver version for your specific GPU.
+    package = config.boot.kernelPackages.nvidiaPackages.stable;
+  };
+
+  # nVidia Prime - 
+  hardware.nvidia.prime = {
+    #sync.enable = true;            # Note: Cannot enable Sync and Offload modes simultaneously
+		offload = {
+			enable = true;
+			enableOffloadCmd = true;
+    # Make sure to use the correct Bus ID values for your system!
+    nvidiaBusId = "PCI:14:0:0";     # nVidia GPU
+    intelBusId = "PCI:0:2:0";       # Intel GPU (Integrated)
+    # amdgpuBusId = "PCI:54:0:0";   # AMD GPU
+    };
+  };
+
+# Lenovo Legion - Power Management, Fan Control, RGB, etc.
+  linuxKernel.packages.linux_latest_libre.lenovo-legion-module
 
 # System Settings
 
